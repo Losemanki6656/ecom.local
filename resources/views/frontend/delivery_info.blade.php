8d838479faf687503cfa4e8f1f7cf7c0790e72cb
Losemanki6656 <bbiznestg@gmail.com>
2023-06-23 15:03:06 +0500

    asd

--- resources/views/frontend/delivery_info.blade.php
+++ resources/views/frontend/delivery_info.blade.php
@@ -533,6 +533,7 @@
         }
 
         function openStreetMap() {
+            var L = window.L;
             var map = L.map('map');
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
